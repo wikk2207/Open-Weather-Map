e35ee39656bed480f7c116332c8ff89bb85aab02
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-05-15 01:29:46 +0200

    Fix eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CityWeatherContainer from './views/CityWeather'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <CityWeatherContainer />
+    </div>
+  )
 }
 
 export default App
