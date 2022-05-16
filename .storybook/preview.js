f5ae6748fa826939aa9621554baf3252135dd099
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-05-16 18:14:26 +0200

    Setup storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import GlobalStyles from '../src/styles/GlobalStyle'
+import '../src/styles/icons.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+}
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
