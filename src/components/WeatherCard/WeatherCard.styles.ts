dbe564ee1a90490f29a1541d194ebbb2d0a6ec7e
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-05-16 23:15:59 +0200

    Add styles to WeatherCard

--- src/components/WeatherCard/WeatherCard.styles.ts
+++ src/components/WeatherCard/WeatherCard.styles.ts
@@ -1,15 +1,27 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  width: 200px;
+  width: 250px;
   border-radius: 5px;
   padding: 5px;
   background-color: white;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
 `
 
-export const DateWrapper = styled.p`
+export const Header = styled.div`
   width: 100%;
   border-bottom: 1px grey solid;
   text-align: center;
+  padding: 5px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ForecastDataWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 `
--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react'
+import moment from 'moment'
 import IconWithValue from 'components/IconWithValue'
 import { IconClassName } from 'styles/icons'
 import { Unit } from 'utils/constants'
-import { Wrapper, DateWrapper } from './WeatherCard.styles'
+import { Wrapper, Header, ForecastDataWrapper } from './WeatherCard.styles'
 
 interface Props {
   date: Date
@@ -10,15 +11,50 @@ interface Props {
   tempDay: number
   tempNight: number
   humidity: number
+  additionalHeaderComponent?: JSX.Element
 }
 
-const WeatherCard: FC<Props> = ({ date, tempMorning, tempDay, tempNight, humidity }) => (
+const WeatherCard: FC<Props> = ({
+  date,
+  tempMorning,
+  tempDay,
+  tempNight,
+  humidity,
+  additionalHeaderComponent
+}) => (
   <Wrapper>
-    <DateWrapper>{`${date.getDate()}.${date.getMonth() + 1}`}</DateWrapper>
-    <IconWithValue icon={IconClassName.Sunrise} value={tempMorning} unit={Unit.DegreeCelsius} />
-    <IconWithValue icon={IconClassName.Sun} value={tempDay} unit={Unit.DegreeCelsius} />
-    <IconWithValue icon={IconClassName.Moon} value={tempNight} unit={Unit.DegreeCelsius} />
-    <IconWithValue icon={IconClassName.Humidity} value={humidity} unit={Unit.Percent} />
+    <Header>
+      <>
+        {`${moment(date).format('dddd, Do MMM')}`}
+        {additionalHeaderComponent}
+      </>
+    </Header>
+    <ForecastDataWrapper>
+      <IconWithValue
+        icon={IconClassName.Sunrise}
+        value={tempMorning}
+        unit={Unit.DegreeCelsius}
+        label="Morning temperature"
+      />
+      <IconWithValue
+        icon={IconClassName.Sun}
+        value={tempDay}
+        unit={Unit.DegreeCelsius}
+        label="Day temperature"
+      />
+      <IconWithValue
+        icon={IconClassName.Moon}
+        value={tempNight}
+        unit={Unit.DegreeCelsius}
+        label="Night temperature"
+      />
+      <IconWithValue
+        icon={IconClassName.Humidity}
+        value={humidity}
+        unit={Unit.Percent}
+        label="Humidity"
+      />
+    </ForecastDataWrapper>
   </Wrapper>
 )
 
